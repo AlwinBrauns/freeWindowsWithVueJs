df19454fd7eda089e17de12b394057fe50ca1ac4
AlwinBrauns <musicproductionbrauns@gmail.com>
2021-03-06 21:10:03 +0100

    id lift

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <window></window>
+  <window :window-id="generateID()"></window>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    generateID(){
+      let myID = (Math.random()*(200)).toString();
+      myID = myID.replace(/\./g,'Z');
+      myID = myID.replace(/[0]/g,'a');
+      myID = myID.replace(/[1]/g,'c');
+      myID = myID.replace(/[2]/g,'x');
+      myID = myID.replace(/[3]/g,'f');
+      myID = myID.replace(/[4]/g,'w');
+      myID = myID.replace(/[5]/g,'o');
+      myID = myID.replace(/[6]/g,'k');
+      myID = myID.replace(/[7]/g,'H');
+      myID = myID.replace(/[8]/g,'U');
+      myID = myID.replace(/[9]/g,'p');
+      return myID;
+    }
+  },
 }
 </script>
 
--- src/components/window/Window.vue
+++ src/components/window/Window.vue
@@ -2,7 +2,7 @@
 <div class="window" :style="styleCSS">
   <window-header
     @change-pos="changePos"
-    :window-header-id="id"
+    :window-header-id="windowId"
     @delete-me="deleteWindow"
     @mousedown="differenceBetweenClickAndElement"
     @touchstart="differenceBetweenClickAndElement"
@@ -15,21 +15,7 @@
 
 <script>
 export default {
-    created: function(){
-        let myID = (Math.random()*(200)).toString();
-            myID = myID.replace(/\./g,'Z');
-            myID = myID.replace(/[0]/g,'a');
-            myID = myID.replace(/[1]/g,'c');
-            myID = myID.replace(/[2]/g,'x');
-            myID = myID.replace(/[3]/g,'f');
-            myID = myID.replace(/[4]/g,'w');
-            myID = myID.replace(/[5]/g,'o');
-            myID = myID.replace(/[6]/g,'k');
-            myID = myID.replace(/[7]/g,'H');
-            myID = myID.replace(/[8]/g,'U');
-            myID = myID.replace(/[9]/g,'p');
-        this.id = myID;
-    },
+    props: ['windowId'],
     data() {
         return {
             x: 0,
@@ -37,7 +23,6 @@ export default {
             dx: 0,
             dy: 0,
             width: 300,
-            id: 0,
         }
     },
     computed: {
